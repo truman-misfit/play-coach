6d314e61378fc7c3959ac809d4f4b4480de58fe2
曾达 <johnz@misfit.com>
2015-09-02 16:05:50 +0800

    [MC-1506]Add a compose action, /composeAction

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,24 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import scala.concurrent._
 
 class Application extends Controller {
 
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
-
+  
+  object LoggingAction extends ActionBuilder[Request] {
+    def invokeBlock[A](request: Request[A], block: (Request[A]) => Future[Result]) = {
+      Logger.info("Calling action")
+      Logger.info("maybe you want to do more things here")
+      //do more thing
+      block(request)
+    }
+  }
+  
+  def composeAction = LoggingAction {
+      Ok("This is a compose Action,you just write a log")
+  }
 }
