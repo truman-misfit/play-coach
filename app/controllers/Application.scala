c285e67715d96eed58423dd22c49e207ea755cfa
曾达 <johnz@misfit.com>
2015-09-02 17:43:11 +0800

    [MC-1506]Add a post action,add a form to post

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,11 +3,22 @@ package controllers
 import play.api._
 import play.api.mvc._
 import scala.concurrent._
+import play.api.data.Form
+import play.api.data.Forms._
+import play.api.i18n._
+import play.api.Play.current
+import play.api.i18n.Messages.Implicits._
 
-class Application extends Controller {
+class Application extends Controller{
+
+  val dataForm: Form[MyDataForm] = Form{
+    mapping(
+      "data" -> nonEmptyText
+    )(MyDataForm.apply)(MyDataForm.unapply)
+  }
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index(dataForm))
   }
   
   object LoggingAction extends ActionBuilder[Request] {
@@ -22,4 +33,18 @@ class Application extends Controller {
   def composeAction = LoggingAction {
       Ok("This is a compose Action,you just write a log")
   }
+
+  def postData = Action { implicit request =>
+    println("request is :" + request.toString)
+    dataForm.bindFromRequest.fold(
+      formWithErrors => Ok("commit error"),
+      {
+        mydata => Ok("receive a data:" + mydata.input)
+      }
+    )
+  }
 }
+
+/*class for the from,I don't know other way to post a request*/
+
+case class MyDataForm(input: String)
