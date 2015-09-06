1c8b5e5b9efcbe900d7f26f1fd0b0cd99d73616f
曾达 <johnz@misfit.com>
2015-09-06 09:16:29 +0800

    [MC-1507]Add an async action,	/asynAct or /asynAct?time=5000

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -8,6 +8,7 @@ import play.api.data.Forms._
 import play.api.i18n._
 import play.api.Play.current
 import play.api.i18n.Messages.Implicits._
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
 
 class Application extends Controller{
 
@@ -42,6 +43,17 @@ class Application extends Controller{
         myData => Ok("receive a data:" + myData.input)
       }
     )
+  }
+  
+  def calWithSleep(time: Int) : Int= {
+      Thread sleep time
+      time
+  }
+  
+  //asyn action,sleep for time microseconds,and then return the time
+  def asynAct(time: Int) = Action.async {request =>
+    val futureInt = scala.concurrent.Future { calWithSleep(time) }
+    futureInt.map(i => Ok("Got result: " + i))
   }
 }
 
